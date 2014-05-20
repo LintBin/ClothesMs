7f546850e2a2300ee80027e02a7d4ec3b712a455
LintBin <312763889@qq.com>
2014-05-20 22:50:31 +0800

    创建项目

---
+++ src/main/java/com/action/AdminAction.java
@@ -0,0 +1,31 @@
+package com.action;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.vo.User;
+
+
+@Component("admin")
+@Scope("prototype")
+public class AdminAction extends ActionSupport{
+	public User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		System.out.println(">>>>>>" + user.getUsername());
+		System.out.println(">>>>>>" + user.getPassword());
+		System.out.println("Action is running");
+		return null;
+	}
+	
+}
---
+++ src/main/java/com/entity/Admin.java
@@ -0,0 +1,48 @@
+package com.entity;
+
+public class Admin {
+	private int Id;
+	private String username;
+	private String password;
+	private String name;
+	private int flag;
+	private String introduction;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getFlag() {
+		return flag;
+	}
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	
+}
